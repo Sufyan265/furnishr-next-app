6872d2e2e418a477e283d469584bd8e2461f6db4
Muhammad Arslan Ramzan <debugmediaplay@gmail.com>
2025-11-26 19:30:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4707434629-bbde2",
+  "appId": "1:339401475120:web:d990d78098badd6b0999f5",
+  "apiKey": "AIzaSyBnhlK4yTN-a2IeZAuPTBBNCbFi2Pfr2PM",
+  "authDomain": "studio-4707434629-bbde2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "339401475120"
+};
