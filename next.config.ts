4769a846c117ddf90ab21ecb7310d00eb0015ed5
Sufyan265 <sufyansaleem26@gmail.com>
2025-12-07 22:10:17 +0500

    update Next version, change hero urls

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
